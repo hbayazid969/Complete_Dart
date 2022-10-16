e983b384c2cfbe6423505231cf81d56e29e4f063
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 01:47:26 +0600

    Create 1.data_type.dart

---
+++ 1.data_type.dart
@@ -0,0 +1,12 @@
+void main() {
+	int age = 18;
+  double height = 5.5;
+  String name = 'Hasan';
+  bool isActive = true;
+
+  print('My age is : $age');
+  print('My height is : $height');
+  print('My name is : $name');
+  print('My active status is : $isActive');
+  
+}
