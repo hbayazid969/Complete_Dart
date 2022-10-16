c3456a94f65039f12d07ba96b86dd2f4ab4de7ac
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 02:05:06 +0600

    Create 2.String.dart

---
+++ 2.String.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  //String Literals Example
+  String s1 = "Hello";
+  String s2 = 'it\'s easy';
+  String s3 = "it's easy";
+  String s4 = "My name is String."
+              "I am a sequence of character";
+  
+  //String interpolation Example
+  String name = "Hasan";
+  print("My name is $name");
+
+  print("There are ${name.length} character in my name");
+}
