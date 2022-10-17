253bfb9f94391655487884b2ff4a664d44ddb989
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 11:29:16 +0600

    Create 3.play_with_string.dart

---
+++ 3.play_with_string.dart
@@ -0,0 +1,37 @@
+//print ascii value for characters
+
+void main(){
+  String s = 'hello';
+  print(s.codeUnits);
+  print(s.codeUnitAt(0));
+  print(s.runes); //Similar
+}
+
+//some impoprtant string methods
+
+void main(){
+  String s = '     life goal    ';
+  String s1 = 'hello';
+  String s2 = 'WORLD';
+  print(s1.isEmpty);
+  print(s1.toUpperCase());
+  print(s2.toLowerCase());
+  print(s1.contains('e'));
+  print(s1.startsWith('h'));
+  print(s1.endsWith('o'));
+  print(s.trim()); //trim whitespace from start & end
+  print(s.trimLeft()); //trim whitespace left
+  print(s.trimRight()); //trim whitespace right
+  print(s.replaceAll('a','o')); //replace word
+}
+
+//reverse a string
+
+String reverseStringUsingCodeUnits(String s) {
+  return String.fromCharCodes(s.codeUnits);
+}
+
+void main() {
+  var s = 'hello';
+  print(reverseStringUsingCodeUnits(s));
+}
