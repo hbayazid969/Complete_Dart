962adcf54c7775c1c2a3772926fcebcda69abbbb
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 15:14:42 +0600

    Update 10.final_and_const.dart

--- 10.final_and_const.dart
+++ 10.final_and_const.dart
@@ -1,5 +1,6 @@
 /*
 Most important thing is that,
+  If we are initialize a value using final & const then we are not able to change it later.
   we have to provide the const variable value before compile our program. Otherwise you get an error.
   If we are are using the final keyword it will allow us to run the program and change the value in runtime.
 */
