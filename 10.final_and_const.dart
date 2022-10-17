a1685697f236840a058571a63d0a54459e49a7d4
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 15:11:36 +0600

    Create 10.final_and_const.dart

---
+++ 10.final_and_const.dart
@@ -0,0 +1,24 @@
+/*
+Most important thing is that,
+  we have to provide the const variable value before compile our program. Otherwise you get an error.
+  If we are are using the final keyword it will allow us to run the program and change the value in runtime.
+*/
+
+
+//Example1
+void main(){
+  final int a=5;
+  const int b=10;
+  
+  a=6;//not possible bro
+  b=11;//not possible bro
+}
+
+
+//Example2
+void main(){
+  final int a;
+  const int b;
+  a=4;//possible
+  b=4;// not possible
+}
