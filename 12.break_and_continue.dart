e3dce6679a1cd580d87d26a82d034d3e54118f78
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 15:51:47 +0600

    Create 12.break_and_continue.dart

---
+++ 12.break_and_continue.dart
@@ -0,0 +1,22 @@
+//practice
+
+//Dart break 
+void main(){
+  for(int i=1;i<=5;i++){
+    if(i==3){
+      break;
+    }
+    print(i);
+  }
+}
+
+
+//Dart continue 
+void main(){
+  for(int i=1;i<=5;i++){
+    if(i==3){
+      continue;
+    }
+    print(i);
+  }
+}
