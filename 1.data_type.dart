9dc9816b103c06c098541c915c6b90bca5dc1bd1
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 11:19:08 +0600

    Update 1.data_type.dart

--- 1.data_type.dart
+++ 1.data_type.dart
@@ -1,9 +1,17 @@
 void main() {
+  
+  //declare integer value
   int age = 18;
+  
+  //declare double value
   double height = 5.5;
+  
+  //declare String value
   String name = 'Hasan';
+  
+  //declare bool value
   bool isActive = true;
-
+  
   print('My age is : $age');
   print('My height is : $height');
   print('My name is : $name');
