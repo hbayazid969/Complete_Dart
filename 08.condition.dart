198c15f6720db24a2a9d0a98b5a90f09e07c87fd
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-17 15:00:05 +0600

    Create 08.condition.dart

---
+++ 08.condition.dart
@@ -0,0 +1,61 @@
+//practice
+
+//Dart ternary 
+void main(){
+  int a,b;
+  a=10;
+  b=5;
+  (a>b)?print('$a is big') : print('$b is big');
+}
+
+//Dart if-else 
+void main(){
+  int a,b;
+  a=10;
+  b=5;
+  if(a>b){
+    print('Yes');
+  }
+  else{
+    print('No');
+  }
+}
+
+
+//Dart if-else & else-if
+void main(){
+  int a,b;
+  a=10;
+  b=10;
+  if(a>b){
+   print('Yes');
+  }
+  else if(a==b){
+    print('Equal');
+  }
+  else{
+    print('No');
+  }
+}
+
+
+//Dartswitch-case
+void main() { 
+   var grade = "A"; 
+   switch(grade) { 
+      case "A": {  print("Excellent"); } 
+      break; 
+     
+      case "B": {  print("Good"); } 
+      break; 
+     
+      case "C": {  print("Fair"); } 
+      break; 
+     
+      case "D": {  print("Poor"); } 
+      break; 
+     
+      default: { print("Invalid choice"); } 
+      break; 
+   } 
+} 
