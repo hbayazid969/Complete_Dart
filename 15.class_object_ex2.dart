1a8b53c1a9c2a9078a00d6051e283775ae7653a7
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-18 19:49:38 +0600

    Create 15.class_object_ex2.dart

---
+++ 15.class_object_ex2.dart
@@ -0,0 +1,17 @@
+//practice class and object
+
+void main(){
+  var f1 = Fight();
+  f1.ready();
+ 
+}
+
+class Fight{
+  int army = 500;
+  int gun = 1000;
+  
+  void ready(){
+    print(gun+army);
+  }
+ 
+}
